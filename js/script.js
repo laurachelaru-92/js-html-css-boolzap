8702292d17291db38beb2c30481e70bc4de1ad2c
Laura Chelaru <laura.a.chelaru@gmail.com>
2020-09-08 19:48:13 +0200

    milestone inserimento fumetto verde completata

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
 $(document).ready(function(){
 
-// Evento al click su "send"
-$("#typing-container .send").click(
-  function() {
-    // Mettiamo il contenuto dell'input in una variabile
-    var messaggio = $("#typing-container input").val();
-    // Cloniamo il templace con balloon verde
-    var balloonVerde = $("template-green").clone();
-    // Rimuoviamo la classe "display: none"
-    // balloonVerde.removeClass("d-none");
-    // Appendere il messaggio nel paragrafo del template
-    $("template-green .green-balloon p").text(messaggio);
-    // Appendere il balloon verde alla finestra di conversazione
-    $("#conversation-window").append(balloonVerde);
+// Al click su "send" viene richiamata la funzione newGreenBalloon
+$("#typing-container .send").click(newGreenBalloon);
+
+// Al tasto "invio" viene richiamata la funzione newGreenBalloon
+$("#typing-container").keypress(
+  function(event) {
+    if(event.which == 13) {
+      newGreenBalloon();
+    }
   }
 );
+
+// Definizione della funzione che crea un nuovo balloon verde
+function newGreenBalloon() {
+  // Cloniamo il templace con balloon verde
+  var balloonVerde = $(".template-green .green-balloon").clone();
+  // Mettiamo il contenuto dell'input in una variabile
+  var messaggio = $("#typing-container input").val();
+
+  if(messaggio != "") {
+    // Appendiamo il messaggio nel paragrafo del balloon verde
+    balloonVerde.children("p").text(messaggio);
+    // Appendiamo il balloon verde nella finestra di conversazione
+    $("#conversation-window").append(balloonVerde);
+    // Puliamo il valore di input
+    $("#typing-container input").val("");
+  }
+}
+
+// Al focus sull'input, 
 
 
 
