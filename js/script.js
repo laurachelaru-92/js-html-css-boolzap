13fd5be921663d1bf17da4c6c8aefbce7b78b21c
Laura Chelaru <laura.a.chelaru@gmail.com>
2020-09-08 17:43:27 +0200

    primo tentativo di clone + append

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
 
-
+// Evento al click su "send"
+$("#typing-container .send").click(
+  function() {
+    // Mettiamo il contenuto dell'input in una variabile
+    var messaggio = $("#typing-container input").val();
+    // Cloniamo il templace con balloon verde
+    var balloonVerde = $("template-green").clone();
+    // Rimuoviamo la classe "display: none"
+    // balloonVerde.removeClass("d-none");
+    // Appendere il messaggio nel paragrafo del template
+    $("template-green .green-balloon p").text(messaggio);
+    // Appendere il balloon verde alla finestra di conversazione
+    $("#conversation-window").append(balloonVerde);
+  }
+);
 
 
 
