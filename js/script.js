a8e052bc942583318ba1b1838ff9bd77608d4306
Laura Chelaru <laura.a.chelaru@gmail.com>
2020-09-09 14:39:25 +0200

    sort all'input su "cerca" in lavorazione

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@ $("#typing-container .send").click(
     // Viene richiamata la funzione newGreenBalloon
     newGreenBalloon();
     // Dopo un secondo, il testo nel header diventa "sta scrivendo..."
-    setTimeout(staScrivendo,1000);
+    setTimeout(staScrivendo,800);
     // Dopo un secondo, inviamo la risposta
-    setTimeout(risposta, 1000);
+    setTimeout(risposta, 1500);
+    // Segniamo l'ultimo accesso nell'header
+    setTimeout(ultimoAccesso, 1800);
   });
 
 // Al tasto "invio" viene richiamata la funzione newGreenBalloon
@@ -18,14 +20,16 @@ $("#typing-container").keypress(
       // Viene richiamata la funzione newGreenBalloon
       newGreenBalloon();
       // Dopo un secondo, il testo nel header diventa "sta scrivendo..."
-      setTimeout(staScrivendo,1000);
+      setTimeout(staScrivendo,800);
       // Dopo un secondo, inviamo la risposta
-      setTimeout(risposta, 1000);
+      setTimeout(risposta, 1500);
+      // Segniamo l'ultimo accesso nell'header
+      setTimeout(ultimoAccesso, 1800);
     }
   }
 );
 
-// Definizione della funzione che crea un nuovo balloon verde
+// Creiamo la funzione che crea un nuovo balloon verde
 function newGreenBalloon() {
   // Cloniamo il templace con balloon verde
   var balloonVerde = $(".template-green .green-balloon").clone();
@@ -67,12 +71,57 @@ function staScrivendo() {
 
 // Creiamo la funzione che genera un nuovo balloon bianco
 function risposta() {
-  var balloonBianco = $(".white-balloon").clone();
+  var balloonBianco = $(".template-white .white-balloon").clone();
   balloonBianco.children("p").text("ok");
   var currentTime = new Date();
   balloonBianco.children("span").text(currentTime.getHours() + ":" + currentTime.getMinutes());
   $("#conversation-window").append(balloonBianco);
 }
+
+// Creiamo la funzione che cambia il testo nell'header in "ultimo accesso"
+function ultimoAccesso() {
+  var currentTime = new Date();
+  $("main header #last-access").text("Ultimo accesso alle " + currentTime.getHours() + ":" + currentTime.getMinutes());
+  $("#contacts-container .last-message-time").text(currentTime.getHours() + ":" + currentTime.getMinutes());
+}
+
+// Al click su un figlio di "contacts-container", cambia il contenuto della Finestra
+$("#contacts-container .contact").click(
+  function() {
+    var nomeContatto = $(this).find("h4").text();
+    if($("#main-header-text h4").text() != nomeContatto) {
+      $("#main-header-text h4").text(nomeContatto);
+      $("#conversation-window").empty();
+      var balloonsAttuali = $(this).find(".balloons").html();
+      $("#conversation-window").append(balloonsAttuali);
+    }
+  }
+);
+
+
+// Mettiamo i contatti in un array
+var arrayContatti = [];
+$("#contacts-container > .contact").each(
+  function() {
+    arrayContatti.push($(this).find(".contact-name").text());
+  }
+);
+console.log(arrayContatti);
+
+// Definiamo una funzione che riordina in base a un input
+function sortare(arrayGenerico,inputGenerico) {
+  var arrayGiusto = [];
+  for(var i = 0; i < arrayGenerico.length; i++) {
+    arrayGenerico[i]
+    if(arrayGenerico[i].includes(inputGenerico)) {
+      arrayGiusto.push(arrayGenerico[i]);
+    }
+  }
+  return arrayGiusto;
+}
+var inputto = "au";
+console.log(sortare(arrayContatti,inputto));
+
 
 
 });
